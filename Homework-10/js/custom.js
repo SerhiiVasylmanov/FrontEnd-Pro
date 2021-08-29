755cb3b8f01eee1178b3e59da1b97b26dec3387e
SerhiiVasylmanov <84910611+SerhiiVasylmanov@users.noreply.github.com>
2021-08-29 17:07:33 +0300

    Homework_10

--- Homework-10/js/custom.js
+++ Homework-10/js/custom.js
@@ -1,6 +1,6 @@
 // Задача №1. Функция должна найти и вернуть максимальный элемент в массиве.
 
-// Вариант №1. Циклы
+//Вариант №1. Циклы
 function max(arr) {
     var max = arr[0];
     for (var i = 1; i < arr.length; i++) {
@@ -13,9 +13,15 @@ function max(arr) {
 
 // Вариант №2. Рекурсия
 function max(arr) {
-    return arr.reduce((a, b) => a > b ? a : b);
+
+    if (arr.length == 1) {
+        return arr[0];
+    }
+    return Math.max(...arr);
+
 }
 
 console.log(max([8]), 'one element test, must return 8');
 console.log(max([1, 8, 37, 5, 17]), '5 elements test, must return 37');
 console.log(max([8, 17]), '2 elements test, must return 17');
+
