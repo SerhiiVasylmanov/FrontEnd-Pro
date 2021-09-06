cbabb40c0141b29729f4a6374e8fced077b908f3
SerhiiVasylmanov <84910611+SerhiiVasylmanov@users.noreply.github.com>
2021-09-06 13:16:04 +0300

    finish Homework-13

--- Homework-13/custom.js
+++ Homework-13/custom.js
@@ -7,11 +7,14 @@ const errorMessage = 'You have not entered any text';
 let listItem;
 
 function activInput() {
-    if (input.value) {
-
+    if (!input.value) {
+        input.classList.add('input_error');
+        input.placeholder = errorMessage;
+        return;
+    } else {
         listItem = document.createElement('li');
         listItem.className = 'list_item';
-        listItem.innerHTML = `${input.value}`;
+        listItem.innerText = `${input.value}`;
         list.append(listItem);
         input.placeholder = message;
         input.value = '';
@@ -19,9 +22,7 @@ function activInput() {
         if (input.classList.contains('input_error')) {
             input.classList.remove('input_error');
         }
-    } else {
-        input.classList.add('input_error');
-        input.placeholder = errorMessage;
+
     }
 }
 
