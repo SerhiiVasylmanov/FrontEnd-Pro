32971c68ae574f7012211f9c4f3ad64622816959
SerhiiVasylmanov <84910611+SerhiiVasylmanov@users.noreply.github.com>
2021-09-12 16:52:10 +0300

    Homework_16 version2

--- Homework-16/custom.js
+++ Homework-16/custom.js
@@ -4,17 +4,20 @@ const list = document.querySelector('#list');
 const message = input.placeholder;
 const errorMessage = 'You have not entered any text';
 
-btnAdd.addEventListener('click', activateInput);
+btnAdd.addEventListener('click', onButtonAddClick);
 
-function activateInput() {
+let removeEl = document.querySelector(".container");
+
+removeEl.addEventListener("click", onButtonRemoveItem);
+
+function onButtonAddClick() {
     if (!input.value) {
         inputError();
         return
     }
-    addLi();
+    addTodo();
     inputReset();
-    removeItem(listItem);
-    readyItem(listItem);
+    onButtonReadyItem(listItem);
 };
 
 function inputError() {
@@ -28,22 +31,25 @@ function inputReset() {
     input.classList.remove('input_error');
 };
 
-function addLi() {
+function addTodo() {
     listItem = document.createElement('li');
     listItem.className = 'list_item';
-    listItem.innerHTML = `<span class="paragraph">${input.value}</span>` +
-        `<button class="deleteBtn">Удалить</button>` +
-        `<button class="readyBtn">Готово</button>`;
+    listItem.innerHTML = `<span class="paragraph">${input.value}</span>
+                         <button class="deleteBtn">Удалить</button>
+                        <button class="readyBtn">Готово</button>`;
     list.append(listItem);
 };
 
-function removeItem(item) {
-    let removeBtn = item.querySelector('.deleteBtn');
+function onButtonRemoveItem(e) {
+    if (!e.target.classList.contains("deleteBtn")) {
+        return;
+    }
 
-    removeBtn.onclick = () => removeBtn.parentElement.remove();
-};
+    let removeBtn = e.target;
+    removeBtn.closest(".list_item").remove();
+}
 
-function readyItem(item) {
+function onButtonReadyItem(item) {
     let readyBtn = item.querySelector('.readyBtn');
     let paragraph = item.querySelector('.paragraph');
 
