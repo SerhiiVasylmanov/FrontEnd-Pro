b03e61964c444f158c3b3a5d017190f1841ddded
SerhiiVasylmanov <84910611+SerhiiVasylmanov@users.noreply.github.com>
2021-10-27 15:53:16 +0300

    Homework-30

---
+++ Homework-30 Калькулятор на NodeJS/app.js
@@ -0,0 +1,9 @@
+const calculator = require('./calculator');
+
+console.log(calculator.add(10, 2)) // 12
+console.log(calculator.sub(12, 5)) // 7
+console.log(calculator.mult(7, 4)) // 28
+console.log(calculator.div(28, 2)) // 14
+console.log(calculator.div(36, 2)) // 18
+console.log(calculator.add(28, 2)) // 30
+console.log(calculator.sub(18, 2)) // 16
